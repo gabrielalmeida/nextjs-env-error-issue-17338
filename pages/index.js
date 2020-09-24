0c30d67c1bf480cd3872d86eff86f5549711e1ec
Gabriel Almeida <gabriel_almeida@me.com>
2020-09-24 12:28:37 -0300

    Add .env and .env.local and test which one is printed

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
+console.log(process.env.NEXT_PUBLIC_TEST, process.env.TEST)
+
 export default function Home() {
   return (
     <div className={styles.container}>
